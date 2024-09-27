e5f414bf44f3458a640f8c3853577065196df9a8
Jaimie <jaimie.j.hemmings@gmail.com>
2024-09-28 00:18:04 +0100

    add: content to services page

--- portfolio_app/settings.py
+++ portfolio_app/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "abcdefg")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
